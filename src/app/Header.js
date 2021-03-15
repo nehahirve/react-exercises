bdb730f8c5bbcfd78d0bd344d5fd2e2c6b3ce809
nehahirve <neha.hirve@hyperisland.se>
2021-03-15 10:24:53 +0100

    username in header

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from './UserContext'
 
 export default function Header() {
+  const { user } = useContext(UserContext)
   return (
-    <header>Welcome!</header>
+    <header>
+      Welcome, {user.firstName} {user.lastName}!
+    </header>
   )
 }
